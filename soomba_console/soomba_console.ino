8ccd256abbb16dee02fe9de7debd8a4de7b6afae
Samuel Grayson <sam@samgrayson.me>
2018-09-15 17:21:25 -0500

    minor changes on arduino side

--- soomba_console/soomba_console.ino
+++ soomba_console/soomba_console.ino
@@ -1,5 +1,4 @@
 #include "ir_def.h"
-#include "IRLibAll.h"
 #include <IRLibSendBase.h>    //We need the base code
 #include <IRLib_HashRaw.h>    //Only use raw sender
 
@@ -10,16 +9,17 @@ const int BAUD_RATE = 9600;
 IRsendRaw sender;
 
 /* RECEIVING CODE
+#include "IRLibAll.h"
 IRrecvPCI myReceiver(2);
 IRdecode decoder;
 */
 
 void setup() {
 	Serial.begin(BAUD_RATE);
-  delay(2000); while (!Serial);
+	delay(2000); while (!Serial);
   
-  // receiver.enableIRIn(); // Start the receiver
-  Serial.println(F("Ready to receive IR signals"));
+	// receiver.enableIRIn(); // Start the receiver
+	Serial.println("ready");
 }
 
 ir_code string2code(String command) {
@@ -37,53 +37,55 @@ ir_code string2code(String command) {
 }
 
 void sendCommand(char * command) {
-    ir_code code = string2code(command);
-    if (code != NULL) {
-      Serial.print("Executing: ");
-      Serial.println(command);
-      sender.send(code, CODE_SIZE,KHZ);//Pass the buffer,length, optionally frequency
-    } else {
-      Serial.print("Don't know how to: ");
-      Serial.println(command);
-    }
+	ir_code code = string2code(command);
+	if (code != NULL) {
+		Serial.print("good ");
+		Serial.println(command);
+		sender.send(code, CODE_SIZE, KHZ);
+	} else {
+		Serial.print("bad ");
+		Serial.print("unknown_command_");
+		Serial.print(command);
+		Serial.println();
+	}
 }
 
 void simpleTest() {
-  while (true) {
-    for(int i = 0; i < 6; i++) {
-      sendCommand("up");
-      delay(200);
-    }
-    for(int i = 0; i < 2; i++) {
-      sendCommand("right");
-      delay(200);
-    }
-    for(int i = 0; i < 3; i++) {
-      sendCommand("up");
-      delay(200);
-    }
-    for(int i = 0; i < 6; i++) {
-      sendCommand("left");
-      delay(200);
-    }
-  }
+	while (true) {
+		for(int i = 0; i < 6; i++) {
+			sendCommand("up");
+			delay(200);
+		}
+		for(int i = 0; i < 2; i++) {
+			sendCommand("right");
+			delay(200);
+		}
+		for(int i = 0; i < 3; i++) {
+			sendCommand("up");
+			delay(200);
+		}
+		for(int i = 0; i < 6; i++) {
+			sendCommand("left");
+			delay(200);
+		}
+	}
 }
 
 void loop() {
-  // simpleTest();
+	// simpleTest();
 
-  // Keep receiving commands
+	// Keep receiving commands
 	if (Serial.available()) {
 		String command = Serial.readStringUntil('\n');
 	}
 
-  /*
-  RECEIVING CODE (removed to save RAM)
-  // Continue looping until you get a complete signal received
-  if (myReceiver.getResults()) {
-    myDecoder.decode();           //Decode it
-    Serial.println("COMPLEX:");
-    myDecoder.dumpResults(true);  //Now print results. Use false for less detail
-    myReceiver.enableIRIn();      //Restart receiver
-  }*/
+	/*
+	  RECEIVING CODE (removed to save RAM)
+	  // Continue looping until you get a complete signal received
+	  if (myReceiver.getResults()) {
+	  myDecoder.decode();           //Decode it
+	  Serial.println("COMPLEX:");
+	  myDecoder.dumpResults(true);  //Now print results. Use false for less detail
+	  myReceiver.enableIRIn();      //Restart receiver
+	  }*/
 }
---
+++ soomba_console/upload.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# uses the board/port prefs from the last GUI session
+arduino --upload soomba_conosle.ino

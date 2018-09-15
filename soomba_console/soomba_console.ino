f4e692cd6a93e972e4f87d2d216deb57ac329359
patil215 <neilpatil215@gmail.com>
2018-09-15 15:18:01 -0400

    Clean up code and remove unnecessary modules

--- soomba_console/soomba_console.ino
+++ soomba_console/soomba_console.ino
@@ -7,15 +7,18 @@ typedef uint16_t* ir_code;
 const int KHZ = 38;
 const int BAUD_RATE = 9600;
 
+IRsendRaw sender;
+
+/* RECEIVING CODE
 IRrecvPCI myReceiver(2);
-IRdecode myDecoder;
-IRsendRaw mySender;
+IRdecode decoder;
+*/
 
 void setup() {
 	Serial.begin(BAUD_RATE);
+  delay(2000); while (!Serial);
   
-  delay(2000); while (!Serial); //delay for Leonardo
-  myReceiver.enableIRIn(); // Start the receiver
+  // receiver.enableIRIn(); // Start the receiver
   Serial.println(F("Ready to receive IR signals"));
 }
 
@@ -33,25 +36,54 @@ ir_code string2code(String command) {
 	}
 }
 
+void sendCommand(char * command) {
+    ir_code code = string2code(command);
+    if (code != NULL) {
+      Serial.print("Executing: ");
+      Serial.println(command);
+      sender.send(code, CODE_SIZE,KHZ);//Pass the buffer,length, optionally frequency
+    } else {
+      Serial.print("Don't know how to: ");
+      Serial.println(command);
+    }
+}
+
+void simpleTest() {
+  while (true) {
+    for(int i = 0; i < 6; i++) {
+      sendCommand("up");
+      delay(200);
+    }
+    for(int i = 0; i < 2; i++) {
+      sendCommand("right");
+      delay(200);
+    }
+    for(int i = 0; i < 3; i++) {
+      sendCommand("up");
+      delay(200);
+    }
+    for(int i = 0; i < 6; i++) {
+      sendCommand("left");
+      delay(200);
+    }
+  }
+}
+
 void loop() {
+  // simpleTest();
+
+  // Keep receiving commands
 	if (Serial.available()) {
 		String command = Serial.readStringUntil('\n');
-		ir_code code = string2code(command);
-		if (code != NULL) {
-			Serial.print("Doing: ");
-			Serial.println(command);
-      mySender.send(code, CODE_SIZE,KHZ);//Pass the buffer,length, optionally frequency
-		} else {
-			Serial.print("Don't know how to: ");
-			Serial.println(command);
-		}
 	}
 
+  /*
+  RECEIVING CODE (removed to save RAM)
   // Continue looping until you get a complete signal received
   if (myReceiver.getResults()) {
     myDecoder.decode();           //Decode it
     Serial.println("COMPLEX:");
     myDecoder.dumpResults(true);  //Now print results. Use false for less detail
     myReceiver.enableIRIn();      //Restart receiver
-  }
+  }*/
 }

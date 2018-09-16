534b8fe58d4f18cca1905e45ac93b8b861c0c772
Neil Patil <neilpatil215@gmail.com>
2018-09-16 05:37:44 +0000

    Hi

--- soomba_controller/main.py
+++ soomba_controller/main.py
@@ -2,16 +2,32 @@
 
 from comm import SoombaBot
 import contextlib
+import platform
 from time import sleep
-from flask import Flask, request
+import curses
 
-app = Flask(__name__)
+#import keyboard
 
-soomba_bot = SoombaBot()
-sleep(5)
+def main(screen):
+    with contextlib.closing(SoombaBot()) as soomba_bot:
+        sleep(5)
+        while True:
+            key = screen.getkey()
+            if key == 'w': 
+                soomba_bot.execute('up')
+            elif key == 's': 
+                soomba_bot.execute('down')
+            elif key == 'a': 
+                soomba_bot.execute('left')
+            elif key == 'd': 
+                soomba_bot.execute('right')
 
-@app.route("/")
-def hello():
-    command = request.args.get('command')
-    soomba_bot.execute(command)
-    return "waddup"
+if __name__ == '__main__':
+    if platform.system() == 'Windows':
+        # this is necessary (albeit proactive) because on Windows, if
+        # there was an error, it would immediately exit and we would
+        # have no way of debugging.
+        import atexit
+        atexit.register(lambda: input('Press enter to exit\n'))
+    curses.wrapper(main)
+

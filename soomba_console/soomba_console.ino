d36c4d43886e4157134b883642eb95df98b0a8e5
patil215 <neilpatil215@gmail.com>
2018-09-16 04:04:00 -0400

    Turn if close to crashing into wall

--- soomba_console/soomba_console.ino
+++ soomba_console/soomba_console.ino
@@ -11,6 +11,10 @@ const int EYES_PIN = 6;
 const int BASE_PIN = 5;
 bool TURN_EYE_ON = false;
 
+int trigPin = 11;    //Trig - green Jumper
+int echoPin = 12;    //Echo - yellow Jumper
+long duration, cm, inches;
+
 IRsendRaw sender; // Defaults to pin 3
 Adafruit_NeoPixel eyeStrip = Adafruit_NeoPixel(24, EYES_PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel baseStrip = Adafruit_NeoPixel(60, BASE_PIN, NEO_RGBW + NEO_KHZ800);
@@ -32,6 +36,9 @@ void setup() {
 
   baseStrip.begin();
   baseStrip.show();
+  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   
   // receiver.enableIRIn(); // Start the receiver
 	Serial.println("ready");
@@ -79,15 +86,42 @@ void sendEyes(String command) {
       setLeftEye(100,100,100);
       TURN_EYE_ON = true;
     }
-  } else {
-    setLeftEye(50,50,50);
-    setRightEye(50, 50, 50);
   }
 }
 
+int index = 0;
 void sendCommand(char * command) {
 	ir_code code = string2code(command);
 	if (code != NULL) {
+
+      
+    // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+    // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(5);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+   
+    // Read the signal from the sensor: a HIGH pulse whose
+    // duration is the time (in microseconds) from the sending
+    // of the ping to the reception of its echo off of an object.
+    pinMode(echoPin, INPUT);
+    duration = pulseIn(echoPin, HIGH);
+   
+    // convert the time into a distance
+    cm = (duration/2) / 29.1;
+    inches = (duration/2) / 74; 
+
+    index += 1;
+    index %= 30;
+    if (index > 15) {
+      code = string2code("right");
+    } else if (index > 0) {
+      code = string2code("left");
+    }
+
+  
 		sender.send(code, CODE_SIZE, KHZ);
 		Serial.print("good ");
 		Serial.println(command);
